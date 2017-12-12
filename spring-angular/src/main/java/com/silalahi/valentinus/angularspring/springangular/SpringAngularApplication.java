97a4d52808564c61253a59b247bafe0ad9aaf24d
Valentinus Silalahi <valentinus.silalahi@gmail.com>
2017-12-12 13:31:56 +0700

    1. komit pertama

---
+++ spring-angular/src/main/java/com/silalahi/valentinus/angularspring/springangular/SpringAngularApplication.java
@@ -0,0 +1,12 @@
+package com.silalahi.valentinus.angularspring.springangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularApplication.class, args);
+	}
+}
