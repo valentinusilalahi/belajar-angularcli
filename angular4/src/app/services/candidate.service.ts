bb2a1605ff63dc12243a9abac8b02809c6e5381e
Valentinus Silalahi <valentinus.silalahi@gmail.com>
2017-12-13 18:23:18 +0700

    angular 3. add service

---
+++ angular4/src/app/services/candidate.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import {Http, RequestOptions, Headers} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { SearchData } from './searchdata';
+import { Candidate } from '../interfaces/candidate';
+
+const url: string = 'http://localhost:8080/api/candidate';
+
+@Injectable()
+export class CandidateService {
+
+  constructor(private http: Http) {
+    
+      }
+    
+      findAll() {
+        return this.http.get(url)
+          .map(res => res.json())
+          .catch(this.handleError);
+      }
+    
+      findById(id: string) {
+        return this
+          .http
+          .get(url + '/' + id)
+          .map(res => res.json())
+          .catch(this.handleError);
+      }
+    
+      save(candidate: Candidate) {
+        let headers = new Headers ({'Content-Type' : 'application/json', 'Cache-Control' : 'no-cache'});
+        let options = new RequestOptions ({ headers : headers });
+        return this.http.post(url, candidate, options)
+          .map(res => res.json())
+          .catch(this.handleError);
+      }
+    
+      delete(id) {
+        return this.http.delete(url + '/' + id)
+          .map(res => res.json())
+          .catch(this.handleError);
+      }
+    
+      search(search: SearchData) {
+        const header = new Headers ({'Content-Type' : 'application/json', 'Cache-Control' : 'no-cache'});
+        const option = new RequestOptions ({headers: header});
+        return this.http.post(url + '/search', search, option)
+          .map(res => res.json())
+          .catch(this.handleError);
+    
+      }
+    
+      handleError(error) {
+        return Observable.throw(error.json() || 'Server Error');
+      }
+
+}
